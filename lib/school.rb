3beae5c04dae1786ab1fa8fbdf0d60880ceaaeeb
Kish Siva <rodes88@msn.com>
2019-04-23 02:42:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,26 +5,26 @@ class School
   attr_accessor :school,:roster, :student, :add_student, :grade, :sort
   
   def initialize(school_name) 
-    name = school_name
-    roster = {}
+    @name = school_name
+    @roster = {}
   end 
 
   def add_student(student_name, grade)
-    if roster[grade]
-      roster[grade] << student_name
+    if @roster[grade]
+      @roster[grade] << student_name
     else
-      roster[grade] = []
-      roster[grade] << student_name
+      @roster[grade] = []
+      @roster[grade] << student_name
     end
   end
   
   def grade(grade_test)
-    roster[grade_test]
+    @roster[grade_test]
   end 
   
   def sort 
-    roster[9].sort!
-    roster.sort_by {|k, v| [k, v] }.to_h
+    @roster[9].sort!
+    @roster.sort_by {|k, v| [k, v] }.to_h
   end 
 
 end 
